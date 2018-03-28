18d1e3c4e3f11d681a5053cb5cf1cd65fa642c32
Liam Oldfield <liam.oldfield@digital.hmrc.gov.uk>
2018-03-28 17:00:22 +0100

    AMB FINALLY ALL WORING !!!!! HAPPPY EASTER AND GOOD BYE XXX

--- src/main/scala/bank/BankOCR.scala
+++ src/main/scala/bank/BankOCR.scala
@@ -81,29 +81,29 @@ object BankOCR extends App {
     }
   }
 
-  def possibleNumbers(missRead: String) : List[Int] = {
+  def possibleNumbers(missRead: String) : List[String] = {
     numberList
       .map(x => missRead.zip(x)
         .map(l => l._2.compareTo(l._1))
         .filter(x => x !=0))
       .zipWithIndex
       .filter(x =>  x._1 == List() || x._1.length ==1)
-      .map(_._2)
+      .map(_._2.toString)
   }
 
 def aMBProducer(scan: String) : List[String] = {
   val splitNums = numberSplitter(scan)
 
   for {
-    a <- possibleNumbers(splitNums.head).map(_.toString)
-    b <- possibleNumbers(splitNums(1)).map(_.toString)
-    c <- possibleNumbers(splitNums(2)).map(_.toString)
-    d <- possibleNumbers(splitNums(3)).map(_.toString)
-    e <- possibleNumbers(splitNums(4)).map(_.toString)
-    f <- possibleNumbers(splitNums(5)).map(_.toString)
-    g <- possibleNumbers(splitNums(6)).map(_.toString)
-    h <- possibleNumbers(splitNums(7)).map(_.toString)
-    i <- possibleNumbers(splitNums(8)).map(_.toString)
+    a <- possibleNumbers(splitNums.head)
+    b <- possibleNumbers(splitNums(1))
+    c <- possibleNumbers(splitNums(2))
+    d <- possibleNumbers(splitNums(3))
+    e <- possibleNumbers(splitNums(4))
+    f <- possibleNumbers(splitNums(5))
+    g <- possibleNumbers(splitNums(6))
+    h <- possibleNumbers(splitNums(7))
+    i <- possibleNumbers(splitNums(8))
 
   } yield {
     a+b+c+d+e+f+g+h+i
@@ -111,29 +111,27 @@ def aMBProducer(scan: String) : List[String] = {
 }
 
 
-  def apply(scan: String) : String = {
+  def AMBListWriter(scan :String) :String = {
     val numString = scanToString(scan)
     val compare = numString.toList
-    val cSnS = checkSum(numString)
-    cSnS match {
-      case "valid" => "valid"
-      case _ => {
-        val out = aMBProducer(scan).filter(x => checkSum(x) == "valid")
-        val out2 = out.map(x => x.toList)
-        println(out2)
-        val out3 = out2.map(x => x.zip(compare))
-        println(out3)
-        val out4 = out3.filter(x => x.count(x => x._2 != x._1) == 1)
-        println(out4)
-        val out5 = out4.map(x => x.map(x => x._1).mkString)
-        s"$numString AMB $out5"
-      }
+    val validOptions = aMBProducer(scan).filter(x => checkSum(x) == "valid").map(x => x.toList)
+    val singleChangeValidOptions = validOptions.map(x => x.zip(compare)).filter(x => x.count(x => x._2 != x._1) ==1 )
+    val output = singleChangeValidOptions.map(x => x.map(x => x._1).mkString)
+    if (output == Nil)"ILL"
+    else s"$numString AMB ${output.map(_+" ").mkString}"
+  }
 
+  def apply(scan: String) : String = {
+    val numString = scanToString(scan)
+        val cSnS = checkSum(numString)
+    cSnS match {
+      case "valid" => s"$numString valid"
+      case _ => AMBListWriter(scan)
     }
   }
 
   val derek ={
-        " _     _  _  _  _  _  _    \n" +
+        " _  _  _  _  _  _  _  _    \n" +
         "| || || || || || || ||_   |\n" +
         "|_||_||_||_||_||_||_| _|  |"
   }
@@ -143,7 +141,9 @@ def aMBProducer(scan: String) : List[String] = {
       "|_||_||_||_||_||_||_||_||_|\n" +
       "|_||_||_||_||_||_||_||_||_|"
   }
-println(apply(derek2))
+  println(Console.UNDERLINED + s"$derek2" + Console.YELLOW_B + Console.RED)
+  println("      __            /^\\\n    .'  \\          / :.\\   \n   /     \\         | :: \\ \n  /   /.  \\       / ::: | \n |    |::. \\     / :::'/  \n |   / \\::. |   / :::'/\n `--`   \\'  `~~~ ':'/`\n         /         (    \n        /   0 _ 0   \\   \n      \\/     \\_/     \\/  \n    -== '.'   |   '.' ==-    HAPPY EASTER FROM PAN XXX  \n      /\\    '-^-'    /\\    \n        \\   _   _   /             \n       .-`-((\\o/))-`-.   \n  _   /     //^\\\\     \\   _    \n.\"o\".(    , .:::. ,    ).\"o\".  \n|o  o\\\\    \\:::::/    //o  o| \n \\    \\\\   |:::::|   //    /   \n  \\    \\\\__/:::::\\__//    /   \n   \\ .:.\\  `':::'`  /.:. /      \n    \\':: |_       _| ::'/  \n LJO `---` `\"\"\"\"\"` `---`")
+println(Console.RESET +apply(derek2))
 
 
 }
--- src/main/scala/bank/tetse.sc
+++
@@ -1,5 +0,0 @@
-val one = List('a','b','c','d')
-val two = List('a', 'c', 'c','z')
-
-
-one.zip(two).count(x => x._1 != x._2)
